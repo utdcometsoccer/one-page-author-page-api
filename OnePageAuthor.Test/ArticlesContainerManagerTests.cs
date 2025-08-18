d37826f82820775a31a71730bbd227244a77954a
Idaho Edokpayi <idaho@edokpayi.com>
2025-08-18 14:28:05 -0600

    achieved around 62 percent coverage in the OnePageAuthorLib and all tests pass

--- OnePageAuthor.Test/ArticlesContainerManagerTests.cs
+++ OnePageAuthor.Test/ArticlesContainerManagerTests.cs
@@ -7,9 +7,25 @@ namespace OnePageAuthor.Test
     public class ArticlesContainerManagerTests
     {
         [Fact]
+        public async Task EnsureContainerAsync_ReturnsContainer()
+        {
+            var databaseMock = new Mock<Database>();
+            var containerMock = new Mock<Container>();
+            var containerResponseMock = new Mock<ContainerResponse>();
+            containerResponseMock.Setup(cr => cr.Container).Returns(containerMock.Object);
+            databaseMock.Setup(db => db.CreateContainerIfNotExistsAsync(
+                It.IsAny<string>(), It.IsAny<string>(), null, null, CancellationToken.None))
+                .ReturnsAsync(containerResponseMock.Object);
+            var manager = new ArticlesContainerManager(databaseMock.Object);
+            var container = await manager.EnsureContainerAsync();
+            Assert.NotNull(container);
+            Assert.Equal(containerMock.Object, container);
+        }
+
+        [Fact]
         public void ThrowsOnNullArguments()
         {
-           Assert.Throws<ArgumentNullException>(() => new ArticlesContainerManager((Database)null!));
+            Assert.Throws<ArgumentNullException>(() => new ArticlesContainerManager((Database)null!));
         }
     }
 }
